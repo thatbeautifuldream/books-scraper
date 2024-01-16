74ff35f693845a8d226ad0aab65277ddabb6d8d2
Milind Mishra ▲ <milind.mishra4@gmail.com>
2024-01-16 21:34:50 +0530

    [FIX] : rating as a number

--- index.ts
+++ index.ts
@@ -7,10 +7,14 @@ const main = async () => {
   const page = await browser.newPage();
   await page.goto(URL);
   const bookData = await page.evaluate(() => {
+    const convertPrice = (price: string) => {
+      const priceString = price.replace("£", "");
+      return parseFloat(priceString);
+    };
     const books = Array.from(document.querySelectorAll(".product_pod"));
     const data = books.map((book: any) => ({
       title: book.querySelector("h3")?.textContent,
-      price: book.querySelector(".price_color")?.textContent,
+      price: convertPrice(book.querySelector(".price_color")?.textContent),
       imgUrl: book.querySelector("img")?.src,
       rating: book.querySelector(".star-rating")?.classList[1],
     }));
